37a6dff2fe6369d3e859dab2306e0409c2bec1cf
Erman İ <ermanilcin@gmail.com>
2023-09-28 17:08:09 +0300

    Update

---
+++ js/preload.js
@@ -0,0 +1,76 @@
+// Import the necessary Electron components
+const contextBridge = require('electron').contextBridge;
+const ipcRenderer = require('electron').ipcRenderer;
+
+// White-listed channels
+const ipc = {
+  // From render to main
+  'render': {
+    'send': [],
+    // From main to render
+    'receive': [],
+    // From render to main and back again
+    'sendReceive': []
+  }
+};
+
+// Exposed protected methods in the render process
+contextBridge.exposeInMainWorld(
+  // Allowed 'ipcRenderer' methods
+  'ipcRender', {
+    // From render to main
+    send: (channel, args) => {
+      let validChannels = ipc.render.send;
+      if (validChannels.includes(channel)) {
+        ipcRenderer.send(channel, args);
+      }
+    },
+    // From main to render
+    receive: (channel, listener) => {
+      let validChannels = ipc.render.receive;
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.on(channel, (event, ...args) => listener(...args));
+      }
+    },
+    // From render to main and back again
+    invoke: (channel, args) => {
+      let validChannels = ipc.render.sendReceive;
+      if (validChannels.includes(channel)) {
+        return ipcRenderer.invoke(channel, args);
+      }
+    }
+  }
+);
+
+/*
+* Render --> Main
+* ---------------
+* Render:  window.ipcRender.send('channel', data); // Data is optional.
+* Main:    ipcMain.on('channel', (event, data) => { console.log(data); })
+*
+*
+* Main --> Render
+* ---------------
+* Main:    mainWindow.webContents.send('channel', data); // Data is optional.
+* Render:  window.ipcRender.receive('channel', (data) => { console.log(data); });
+*
+*
+* Render --> Main (Value) --> Render
+* ----------------------------------
+* Render:  window.ipcRender.invoke('channel', data).then((result) => { console.log(result); });
+* Main:    ipcMain.handle('channel', (event, data) => { return data; });
+*
+*
+* Render --> Main (Promise) --> Render
+* ------------------------------------
+* Render:  window.ipcRender.invoke('channel', data).then((result) => { console.log(result); });
+* Main:    ipcMain.handle('channel', async (event, data) => {
+*            const myPromise = new Promise((resolve, reject) => {
+*              setTimeout(() => {
+*                resolve({test: 'foo'});
+*              }, 300);
+*            });
+*            return await myPromise.then((result) => { return result; });
+*          });
+*/
